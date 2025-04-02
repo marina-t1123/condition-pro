fd8a9e8be3ccb04d5aa61e57f58d362dc047c1df
marina-t1123 <tm.274795@gmail.com>
2025-04-02 20:06:33 +0900

    feat:選手削除機能実装、update:Athleteモデルメソッド追加

--- app/Models/Athlete.php
+++ app/Models/Athlete.php
@@ -15,7 +15,9 @@ class Athlete extends Model
     protected $fillable = ['team_id', 'sex_id', 'name', 'birthday', 'memo'];
 
     /**
-     * 　選手の性別
+     * 選手の性別譲情報を取得　
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
     public function sex(): BelongsTo
     {
@@ -24,6 +26,8 @@ class Athlete extends Model
 
     /**
      * 選手のチーム
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
     public function team(): BelongsTo
     {
@@ -32,111 +36,119 @@ class Athlete extends Model
 
     /**
      * 選手のポジション・階級
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
      */
-    public function mEventPositions():BelongsToMany
+    public function mEventPositions(): BelongsToMany
     {
         return $this->belongsToMany(MEventPosition::class, 'player_positions', 'athlete_id', 'm_event_position_id')->withTimestamps();
     }
 
     /**
-     * 名前・種目・ポジションでの条件に一致する選手情報を関連情報と一緒に取得
+     * 全選手一覧画面での選手情報を取得する
+     *
      */
-    public static function fetchSearchAthlete($team_id, $search_name, $search_event_id, $search_position_id)
+    public static function fetchAthleteData($request, $teamId)
+    {
+        $searchName = $request->input('athlete_name');
+        $searchEventId = $request->input('m_event_id');
+        $searchPositionId = $request->input('m_event_position_id');
+
+        // dd($searchEventId);
+        $athleteData['athletes'] = Athlete::retrieveAthlete($teamId, $searchName, $searchEventId, $searchPositionId)->get();
+
+        if (!empty($teamId)) {
+            $teamId = intval($teamId, 10);
+            $athleteData['team'] = Team::with('mEvent')->findOrFail($teamId);
+
+            $athleteData['m_event_id'] = $athleteData['team']->mEvent->id;
+            $athleteData['m_event_name'] = $athleteData['team']->mEvent->event_name;
+            $athleteData['m_event_positions'] = $athleteData['team']->mEvent->mEventPositions;
+
+        } else {
+            // 全選手の場合は全種目情報を取得
+            $athleteData['m_events'] = MEvent::getAllMEventAndPositions()->get();
+        }
+
+        return $athleteData;
+    }
+
+    /**
+     * 選手情報の検索
+     * 名前・種目・ポジションでの条件に一致する選手情報を関連情報と一緒に取得
+     *
+     * @param int|null $teamId
+     * @param string|null $searchName
+     * @param int|null $searchEventId
+     * @param int|null $searchPositionId
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public static function retrieveAthlete($teamId, $searchName, $searchEventId, $searchPositionId)
     {
         // 選手のクエリビルダを設定（この時、リレーション関係にあるteamとsexの情報も一緒に取得する）
         $query = Athlete::query()->with(['team', 'sex', 'mEventPositions.mEvent']);
 
         // チームIDが指定された場合
-        if(!empty($team_id)) {
-            $query->whereHas('team', function($query) use ($team_id) {
-                $query->where('id', $team_id);
+        if (!empty($teamId)) {
+            $query->whereHas('team', function ($query) use ($teamId) {
+                $query->where('id', $teamId);
             });
         }
 
         // 選手の名前が検索フォームから検索された場合
-        if(!empty($search_name)) {
-            $query->where('name', 'LIKE', '%'.$search_name.'%');
+        if (!empty($searchName)) {
+            $query->where('name', 'LIKE', '%' . $searchName . '%');
         }
 
         // 種目名が検索フォームから検索された場合
-        if(!empty($search_event_id)) {
-            $query->whereHas('team', function($query) use ($search_event_id) {
-                $query->where('m_event_id', $search_event_id);
+        if (!empty($searchEventId)) {
+            $query->whereHas('team', function ($query) use ($searchEventId) {
+                $query->where('m_event_id', $searchEventId);
+            });
+        }
+
+        // ポジションIDが検索フォームから検索された場合
+        if (!empty($searchPositionId)) {
+            $query->whereHas('mEventPositions', function ($query) use ($searchPositionId) {
+                $query->where('m_event_positions.id', $searchPositionId);
             });
         }
 
         // 種目名、ポジション・階級のそれぞれのIDが送信された場合
-        if(!empty($search_event_id) && !empty($search_position_id)) {
+        if (!empty($searchEventId) && !empty($searchPositionId)) {
 
             // すでに取得済みの選手情報に紐づくポジションを指定条件で取得
-            $query->with(['mEventPositions' => function($query) use($search_event_id, $search_position_id) {
+            $query->with(['mEventPositions' => function ($query) use ($searchEventId, $searchPositionId) {
                 // 指定されたポジションIDのみ取得する
-                $query->where('m_event_positions.id', $search_position_id)
+                $query->where('m_event_positions.id', $searchPositionId)
                     // かつ、そのポジションが指定された種目に属していることを確認
-                    ->whereHas('mEvent', function($query) use ($search_event_id) {
-                        $query->where('id', $search_event_id);
+                    ->whereHas('mEvent', function ($query) use ($searchEventId) {
+                        $query->where('id', $searchEventId);
                     });
 
                 // ポジションに紐づく種目情報も取得する
                 $query->with('mEvent');
-
             }]);
         }
 
-        $search_athletes = $query;
+        $searchAthletes = $query;
 
-        return $search_athletes;
+        return $searchAthletes;
     }
-    // /**
-    //  * 名前・種目・ポジションでの条件に一致する選手情報を関連情報と一緒に取得
-    //  */
-    // public static function featchSearchAthlete($search_name, $search_event_id, $search_position_id)
-    // {
-    //     // 選手のクエリビルダを設定（この時、リレーション関係にあるteamとsexの情報も一緒に取得する）
-    //     $query = Athlete::query()->with(['team', 'sex', 'mEventPositions.mEvent']);
-
-    //     // 選手の名前が検索フォームから検索された場合
-    //     if(!empty($search_name)) {
-    //         $query->where('name', 'LIKE', '%'.$search_name.'%');
-    //     }
-
-    //     // 種目名が検索フォームから検索された場合
-    //     if(!empty($search_event_id)) {
-    //         $query->whereHas('team', function($query) use ($search_event_id) {
-    //             $query->where('m_event_id', $search_event_id);
-    //         });
-    //     }
-
-    //     // 種目名、ポジション・階級のそれぞれのIDが送信された場合
-    //     if(!empty($search_event_id) && !empty($search_position_id)) {
-
-    //         // すでに取得済みの選手情報に紐づくポジションを指定条件で取得
-    //         $query->with(['mEventPositions' => function($query) use($search_event_id, $search_position_id) {
-    //             // 指定されたポジションIDのみ取得する
-    //             $query->where('m_event_positions.id', $search_position_id)
-    //                 // かつ、そのポジションが指定された種目に属していることを確認
-    //                 ->whereHas('mEvent', function($query) use ($search_event_id) {
-    //                     $query->where('id', $search_event_id);
-    //                 });
-
-    //             // ポジションに紐づく種目情報も取得する
-    //             $query->with('mEvent');
-
-    //         }]);
-    //     }
-
-    //     $search_athletes = $query;
-
-    //     return $search_athletes;
-    // }
 
     /**
-     * 対象選手のポジション情報関連の情報をデータ配列にして取得
+     * 対象選手のポジション情報を取得
+     * ポジション関連の情報を取得しやすいでデータ構造の配列にしている
+     *
+     * @param \App\Models\Athlete $athlete 選手情報
+     * @param int $position_id ポジションID
+     * @return array
      */
-    public static function setAthletePositionData($athlete, $position_id) {
+    public static function setAthletePositionData($athlete, $position_id)
+    {
 
         // 対象のplayer_positions(中間テーブル)のIDを取得する
-        if(!empty($athlete->id) && !empty($position_id)) {
+        if (!empty($athlete->id) && !empty($position_id)) {
             $player_position = PlayerPosition::where('athlete_id', $athlete->id)
                 ->where('m_event_position_id', $position_id)
                 ->first();
@@ -145,12 +157,12 @@ class Athlete extends Model
         }
 
         // 選手の種目名を取得する
-        if($player_position_id) {
+        if ($player_position_id) {
             $m_event_position = MEventPosition::findOrFail($position_id);
             $position_name = $m_event_position->event_position_name;
         }
 
-         // 選手データを配列に変換
+        // 選手データを配列に変換
         $athlete_data_array = $athlete->toArray();
 
         // ポジション情報を直接アクセス可能なプロパティとして追加
@@ -161,4 +173,25 @@ class Athlete extends Model
         return $athlete_data_array;
     }
 
+    /**
+     * 削除に必要な情報を取得
+     *
+     * @param int $deleteAthleteId
+     * @return array{ delete_athlete: \App|Models|Athlete, athlete_name: string, positions: \Illuminate\Database\Eloquent\Collection }
+     */
+    public static function getInfoNecessaryDeleting($deleteAthleteId)
+    {
+        $deleteAthlete = Athlete::with('mEventPositions', 'team')->findOrFail($deleteAthleteId);
+
+        $deleteAthleteNama = $deleteAthlete->name;
+        $deleteAthletePositions = $deleteAthlete->mEventPositions()
+            ->wherePivot('athlete_id', $deleteAthlete->id)
+            ->get();
+
+        return [
+            'delete_athlete' => $deleteAthlete,
+            'athlete_name' => $deleteAthleteNama,
+            'positions' => $deleteAthletePositions
+        ];
+    }
 }
